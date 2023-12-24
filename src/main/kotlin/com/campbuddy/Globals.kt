b5368fe99df6a7a0685d87b94ea358a3af3d6c46
gq97a6 <szymon.kogut@protonmail.com>
2023-12-24 16:45:28 +0100

    Domain change.

--- src/main/kotlin/com/campbuddy/Globals.kt
+++ src/main/kotlin/com/campbuddy/Globals.kt
@@ -7,9 +7,19 @@ import io.vertx.mutiny.mysqlclient.MySQLPool
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.event.Observes
 import jakarta.inject.Inject
+import org.eclipse.microprofile.config.inject.ConfigProperty
+
+
+
 
 @ApplicationScoped
 class Globals {
+    @ConfigProperty(name = "server.origin")
+    var origin: String? = null
+
+    @ConfigProperty(name = "server.domain")
+    var domain: String? = null
+
     @Inject
     var vertx: Vertx? = null
 
@@ -20,10 +30,14 @@ class Globals {
     fun onStart(@Observes event: StartupEvent) {
         Companion.mainClient = mainClient
         Companion.vertx = vertx!!
+        Companion.origin = origin ?: "http://localhost"
+        Companion.domain = domain ?: "localhost"
     }
 
     companion object {
         lateinit var mainClient: MySQLPool
         lateinit var vertx: Vertx
+        lateinit var origin: String
+        lateinit var domain: String
     }
 }
